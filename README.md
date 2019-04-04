61e66d40cda82b23ed50088126066d0e53099b6e
YingCGooi <25574844+YingCGooi@users.noreply.github.com>
2019-04-03 21:49:22 -0400

    complete README.md

--- README.md
+++ README.md
@@ -12,27 +12,15 @@
 ## API Documentation
 ### GET `/api/schedules`
 - returns a json containing all schedules and their associated appointments
-- curl example: 
-```
-curl http://localhost:4567/api/schedules
-```
 
 ### GET `/api/schedules/:id`
 - params: id
 - returns a json array containing all appointments in the specified schedule
-- curl example:
-```
-curl http://localhost:4567/api/schedules/0
-```
 - 404 with an error message if the schedule is not found
 
 ### POST `/api/schedules`
 - creates a new schedule with no appointments
 - returns a json object representing the created schedule object
-- curl example:
-```
-curl -X POST http://localhost:4567/api/schedules
-```
 
 ### POST `/schedules/:id/appointments`
 - creates a new appointment on the specified schedule
@@ -48,10 +36,27 @@ curl -X POST -F 'start_time=1' -F 'end_time=2' http://localhost:4567/api/schedul
 ### DELETE `schedules/:id`
 - deletes a schedule with its associated appointments
 - params: id
-- curl example:
-```
+- 404 if schedule is not found
 
-```
+### DELETE `/appointments/:id`
+- deletes an appointment
+- params: id
+- 404 if appointment is not found
+
+### DELETE `/schedules/:schedule_id/appointments/:appt_id`
+- deletes an appointment in the specified schedule
+- params: schedule_id, appt_id
+- returns 404 if appointment is not in specified schedule or not found
+
+### GET `/appointments/:id`
+- returns a single appointment json
+- params: id
+- returns 404 if appointment is not found
+
+### GET `/schedules/:schedule_id/appointments/:appt_id`
+- returns a single appointment json in the specified schedule
+- params: schedule_id, appt_id
+- returns 404 if appointment is not in specified schedule or not found
 
 ## Data Design
 For this project, I use in-memory storage in the form of hash tables. If the requirement states to persist data, we could choose to write the data to a YAML or JSON file, or even use a RDBMs to enable SQL operations. For now, in-memory storage is fine since we're going to focus on the API features.
@@ -73,4 +78,18 @@ Appointment object:
 
 Each appointment object will contain an id attribute to allow easy retrieval and deletion.
 
-## 
+## New Appointment Validation
+All appointments are ordered based on their start and end times.
+When a new appointment is being created, a binary search is performed to determine the position in which it will be inserted.
+A validation is also run to ensure that there are no overlaps.
+
+## Tests
+To run tests on the model layer:
+```
+bundle exec ruby test/model_test.rb
+```
+
+To run tests on the API:
+```
+bundle exec ruby test/api_test.rb
+```
--- test/api_test.rb
+++ test/api_test.rb
@@ -1,1 +1,108 @@
 ENV['RACK_ENV'] = 'test'
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'rack/test'
+require 'json'
+require_relative '../server'
+require_relative '../lib/model'
+
+Minitest::Reporters.use!
+
+class APITest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def save_appointment!(id, start_time, end_time)
+    Schedule.save(id: id, appt: Appointment.new(start_time, end_time))
+  end   
+
+  def setup
+    Schedule.new
+    save_appointment!(0, 1, 2)
+    save_appointment!(0, 6, 7)
+    save_appointment!(0, 100, 200)
+    save_appointment!(0, 201, 202)
+    save_appointment!(0, 240, 260)
+    Schedule.new
+    save_appointment!(1, 1, 2)
+    save_appointment!(1, 3, 4)
+  end
+
+  def teardown
+    Schedule.destroy_all!
+    Schedule.reset_id
+    Appointment.reset_id    
+  end
+
+  def test_get_all_schedules
+    get '/api/schedules'
+    assert_equal 200, last_response.status
+
+    json = JSON.parse(last_response.body)
+    assert_equal 2, json.length
+    assert_equal 5, json['0'].length
+    assert_equal 2, json['1'].length
+  end
+
+  def test_get_appointments_in_a_schedule
+    get '/api/schedules/0'
+    assert_equal 200, last_response.status
+
+    json = JSON.parse(last_response.body)
+    assert_equal 5, json.length
+  end
+
+  def test_create_a_schedule
+    post '/api/schedules'
+    assert_equal 201, last_response.status
+
+    schedules = Schedule.all
+    assert_equal 3, schedules.length
+  end
+
+  def test_create_valid_new_appointment
+    post '/api/schedules/1/appointments', { start_time: 5, end_time: 6 }
+    assert_equal 201, last_response.status
+
+    schedule = Schedule[1]
+    assert_equal 3, schedule.length
+    assert_equal 6, schedule.last.end_time
+  end
+
+  def test_create_invalid_new_appointment
+    post '/api/schedules/1/appointments', { start_time: 5, end_time: 4 }
+    assert_equal 400, last_response.status
+
+    schedule = Schedule[1]
+    assert_equal 2, schedule.length
+  end
+
+  def test_create_overlap_appointment
+    post '/api/schedules/1/appointments', { start_time: 4, end_time: 6 }
+    assert_equal 400, last_response.status
+
+    schedule = Schedule[1]
+    assert_equal 2, schedule.length
+  end
+
+  def test_delete_appointment
+    delete '/api/schedules/1/appointments/5'
+    assert_equal 204, last_response.status
+
+    schedule = Schedule[1]
+    assert_equal 1, schedule.length
+    assert_equal({ id: 6, start_time: 3, end_time: 4 }, schedule.last.to_hash)
+  end
+
+  def test_get_single_appointment
+    get '/api/schedules/1/appointments/5'
+    assert_equal 200, last_response.status
+
+    json = JSON.parse(last_response.body)
+    assert_equal({"id" => 5, "start_time" => 1, "end_time" => 2}, json)
+  end
+end
